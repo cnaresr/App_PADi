8432ce3021de475d850fb1d0df8bf605f7bf2a7a
fariz123133 <farizhikmal94@gmail.com>
2026-06-09 23:16:47 +0700

    fix: sesuaikan huruf besar kecil nama file ejs

--- backend/src/app.js
+++ backend/src/app.js
@@ -56,7 +56,7 @@ app.use('/api/absensi', absensiRoutes);
 
 // Halaman Login Web Admin
 app.get('/', (req, res) => {
-    res.render('login', { error: req.query.error });
+    res.render('Login', { error: req.query.error });
 });
 
 // Proses Login Web Admin (Menembak API Internal)
